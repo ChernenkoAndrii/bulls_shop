2705401d2036909521dca2d88bf0fef0fe2ca608
Andrii <pobedon1@gmail.com>
2018-06-08 12:45:14 +0000

    initial

---
+++ js/index.js
@@ -0,0 +1,14 @@
+var slideIndex = 0;
+showSlides();
+function showSlides(){
+    const slides = document.getElementsByClassName("mySlides");
+    for (let i=0; i<slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 2000);
+}
+const cart = new Cart($('#cartModal'));
+const productList = new ProductList('products.json', $('.products-container'), cart);
